74fb66d294461ef13e80de8e415bf51060be468c
Sabrina Nedjah <snedjah78@gmail.com>
2017-05-24 09:19:27 +0200

    Creation Contact page

---
+++ app/partials/_header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Talo Energy</title>
+  <script defer src="scripts/index.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="shortcut icon" href="images/logoTalo.png" type="image/x-icon" />
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="styles/css/main.css" />
+</head>
+<body>
