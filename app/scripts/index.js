e2c7d1019d12db24ce787917893d35cd68c8cb9d
Sabrina Nedjah <snedjah78@gmail.com>
2017-05-25 17:41:37 +0200

    add Gitignore

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,3 +1,21 @@
-/**
- * Created by raphael on 18/05/2017.
- */
+$(document).ready(function() {
+	console.log('ready!');
+});
+var hamburgerNav = document.querySelector('.hamburgerNav');
+var hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener(
+	'click',
+	function() {
+		hamburger.classList.toggle('is-active');
+		hamburgerNav.classList.toggle('is-active');
+		if (hamburgerNav.classList.contains('is-active')) {
+			hamburgerNav.style.display = 'inline-block';
+		} else {
+			hamburgerNav.style.display = 'none';
+		}
+	},
+	false
+);
+
+console.log('hello');
