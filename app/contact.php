9b74aeaa2999175d911efd800376a50b85723b08
Sabrina Nedjah <snedjah78@gmail.com>
2017-05-26 17:23:24 +0200

    [BACK][CONTACT]add submit value to form

--- app/contact.php
+++ app/contact.php
@@ -14,55 +14,58 @@ require("partials/_nav.php");
   <input type="date" name="eventDate" />
   <label for="content">Message :</label>
   <textarea value="content" name="content" placeholder="Écrivez-Nous !"></textarea>
-  <input id="validation" type="submit" name="submit" value="Envoyer" />
+  <input id="validation" type="submit" name='submit' value="Envoyer" />
 </form>
 
-
 <?php
+
 require_once('phpmailer/PHPMailerAutoload.php');
 
-echo 'je passe par là';
 if (isset($_POST['submit'])) {
-$mail = new PHPMailer();
-$nom = $_POST["nom"];
-$prenom = $_POST["prenom"];
-$usrmail=$_POST["email"];
-$phone=$_POST["phone"];
-$msg=$_POST["content"];
+  $mail = new PHPMailer();
 
-//$mail->isSMTP();
-//$mail->SMTPDebug = 2;                               // Enable verbose debug output
-echo 'ici aussi';
-$mail->isSMTP();
-//$mail->Host = 'localhost';                                   // Set mailer to use SMTP
-$mail->Host = gethostbyname('smtp.gmail.com');// Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'taloenergydev@gmail.com';                 // SMTP username
-$mail->Password = 'Sabrina2017';                           // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
+  $nom = $_POST["nom"];
+  $prenom = $_POST["prenom"];
+  $usrmail = $_POST["email"];
+  $phone = $_POST["phone"];
+  $msg = $_POST["content"];
 
-$mail->setFrom('taloenergydev@gmail.com', 'taloenergydev');
-$mail->addAddress('taloenergydev@gmail.com', 'Information');
-$mail->addReplyTo($usrmail, "$nom $prenom");     // Add a recipient
+  if (empty($usrmail)) {
+    echo 'Please enter your email';
 
+    return;
+  }
 
+  //$mail->isSMTP();
+  //$mail->SMTPDebug = 2;                               // Enable verbose debug output
+                                     // Set mailer to use SMTP
+  $mail->Host = ' smtp.gmail.com; smtp.gmail.com';  // Specify main and backup SMTP servers
+  $mail->SMTPAuth = true;                               // Enable SMTP authentication
+  $mail->Username = 'taloenergydev@gmail.com';                 // SMTP username
+  $mail->Password = 'Sabrina2017';                           // SMTP password
+  $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+  $mail->Port = 587;                                    // TCP port to connect to
 
-$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(false);                                  // Set email format to HTML
-
-$mail->Subject =  "Mail contact ";
-$mail->Body    =  "$nom\n $prenom\n $phone\n $msg";
+  $mail->setFrom('taloenergydev@gmail.com', 'taloenergydev');
+  $mail->addAddress($usrmail, "$nom $prenom");     // Add a recipient
+  $mail->addReplyTo('taloenergydev@gmail.com', 'Information');
 
 
-if(!$mail->send()) {
+  $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+  $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+  $mail->isHTML(false);                                  // Set email format to HTML
+
+  $mail->Subject =  "Mail contact ";
+  $mail->Body    =  "$nom\n $prenom\n $phone\n $msg";
+
+  if(!$mail->send()) {
     echo 'Message could not be sent.';
     echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-  echo "<script>alert(\"Votre message a bien été envoyé !\")</script>";
+  } else {
+    echo "<script>alert(\"Votre message a bien été envoyé !\")</script>";
+  }
+}
 
-}
-}
 ?>
-<?php require("partials/_footer.php"); ?>
+ <?php require("partials/_footer.php");
+ ?>
