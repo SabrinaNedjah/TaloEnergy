e346447764a07ccdeac542507b7e6e197f9319d5
Sabrina Nedjah <snedjah78@gmail.com>
2017-05-27 19:55:26 +0200

    [FRONT][INDEX.PHP]add php and responsive menu

--- app/partials/_nav.php
+++ app/partials/_nav.php
@@ -1,13 +1,23 @@
+<header class="hamburgerHeader">
+  <button class="hamburger hamburger--spring" type="button">
+    <span class="hamburger-box">
+      <span class="hamburger-inner"></span>
+    </span>
+  </button>
+  <span class="titleHeader"> <strong>TALO</strong> ENERGY</span>
+  <div class="hamburgerNav"></div>
+</header>
 
-  <header class="header">
-    <div class="container">
-      <div class="row">
-        <span class="col-md-1"><img src="images/logoTalo.png" class="logoTalo" alt="logo de Talo Energy"></span>
-        <span class="col-md-3"> <strong>TALO</strong> ENERGY</span>
-        <a class="col-md-2 is-active" href="#">Accueil</a>
-        <a class="col-md-2" href="#">L'esprit Talo</a>
-        <a class="col-md-3" href="contact.php">Contactez-nous</a>
-        <a class="sectionPro" href="#"><span>ESPACE PRO</span></a>
-      </div>
+<header class="header">
+  <div class="container">
+    <div class="row">
+      <span class="col-md-1"><img src="images/logoTalo.png" class="logoTalo" alt="logo de Talo Energy"></span>
+      <span class="col-md-3"> <strong>TALO</strong> ENERGY</span>
+      <a class="col-md-2 is-active" href="#">Accueil</a>
+      <a class="col-md-2" href="#">L'esprit Talo</a>
+      <a class="col-md-3" href="#">Contactez-nous</a>
+      <a class="sectionPro" href="#"><span>ESPACE PRO</span></a>
     </div>
-  </header>
+  </div>
+
+</header>
--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,4 +1,19 @@
-/**
- * Created by raphael geloen on 18/05/2017.
- */
- console.log("hello");
+$(document).ready(function() {
+	console.log('ready!');
+});
+var hamburgerNav = document.querySelector('.hamburgerNav');
+var hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener(
+	'click',
+	function() {
+		hamburger.classList.toggle('is-active');
+		hamburgerNav.classList.toggle('is-active');
+		if (hamburgerNav.classList.contains('is-active')) {
+			hamburgerNav.style.display = 'inline-block';
+		} else {
+			hamburgerNav.style.display = 'none';
+		}
+	},
+	false
+);
