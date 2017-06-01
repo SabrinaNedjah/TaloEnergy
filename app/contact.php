a2889a07bf08805de3a698526feb372033048da4
rgeloen <rgeloen@hotmail.fr>
2017-06-01 16:08:38 +0200

    added style contact

--- app/contact.php
+++ app/contact.php
@@ -8,13 +8,17 @@ require("partials/_nav.php");
     <p>Restons en <strong>contact</strong></p>
   </div>
   <section class="formulaire">
-  <form class="form-horizontal" role="form" method="post" action="index.php">
-  	<div class="form-group">
+  <form class="form-horizontal" role="form" method="post" action="contact.php">
+  	<div class="form-group nom">
   		<div class="col">
-  			<input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name" value="">
-          <input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name" value="">
+  			<input type="text" class="form-control nomPrenom" id="name" name="nom" placeholder="Nom" value="">
   		</div>
   	</div>
+    <div class="form-group prenom">
+      <div class="col">
+          <input type="text" class="form-control" id="name" name="prenom" placeholder="Prénom" value="">
+    </div>
+  </div>
   	<div class="form-group">
   		<div class="col-lg">
   			<input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="">
@@ -22,47 +26,20 @@ require("partials/_nav.php");
   	</div>
   	<div class="form-group">
   		<div class="col-lg">
-  			<textarea class="form-control" rows="4" name="message"></textarea>
+  			<textarea class="form-control message" rows="4" name="message"></textarea>
   		</div>
   	</div>
     <div class="form-group">
     		<div class="col-sm-10 col-sm-offset-2">
-    			<input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
+    			<input id="submit" name="submit" type="submit" value="Envoyez" class="btn btn-primary">
     		</div>
     	</div>
       </form>
 </section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!--  <form method="post" action="contact.php">
-    <label for="nom">Nom :</label>
-    <input type="text" name="nom" placeholder="Nom" /><label for="prenom">Prenom :</label><input type="text" name="prenom" placeholder="Prenom" />
-    <label for="email">E-mail :</label>
-    <input id="email" type="email" name="email" placeholder="email" />
-    <label for="phone">Telephone :</label>
-    <input type="text" name="phone" placeholder="+33 6...." />
-    <label for="date">Date de votre événement :</label>
-    <input type="date" name="eventDate" />
-    <label for="content">Message :</label>
-    <textarea value="content" name="content" placeholder="Écrivez-Nous !"></textarea>
-    <input id="validation" type="submit" name='submit' value="Envoyer" />
-  </form> -->
-
+<section class="map"></section>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.3335834604204!2d2.2768014151249574!3d48.87091700777946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66ff63ad6c7d7%3A0x475eed39821352be!2s7+Place+du+Chancelier+Adenauer%2C+75116+Paris!5e0!3m2!1sfr!2sfr!4v1496308523251" margin="90px" width="500" height="320" frameborder="0" style="border:0" allowfullscreen></iframe>
  </section>
+
  <div class="wave"></div>
  <section class="contactContener">
    <div class="contactTitle2">
@@ -81,25 +58,11 @@ require("partials/_nav.php");
        <img src="images/instagram.png" alt="icone de thermomètre">
      </div>
    </div>
+    <p>Par mail, à l’adresse suivante</p>
+<p class="contactTalo">contac@taloenergy.fr</p>
+<p>Et par téléphone au </p>
+ <p class="contactTalo">01 02 03 04 05</p>
  </section>
-
-
-
-
- </section>
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 <?php
@@ -107,12 +70,10 @@ require_once('phpmailer/PHPMailerAutoload.php');
 
 if (isset($_POST['submit'])) {
   $mail = new PHPMailer();
-
   $nom = $_POST["nom"];
   $prenom = $_POST["prenom"];
   $usrmail = $_POST["email"];
-  $phone = $_POST["phone"];
-  $msg = $_POST["content"];
+  $msg = $_POST["message"];
 
   if (empty($usrmail)) {
     echo 'Please enter your email';
@@ -132,7 +93,7 @@ if (isset($_POST['submit'])) {
 
   $mail->setFrom($usrmail, "$nom $prenom" );
   $mail->addAddress('taloenergydev@gmail.com', 'taloenergydev');     // Add a recipient
-  $mail->addReplyTo('taloenergydev@gmail.com', 'Information');
+  $mail->addReplyTo($usrmail, 'Information');
 
 
   $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
@@ -140,7 +101,7 @@ if (isset($_POST['submit'])) {
   $mail->isHTML(false);                                  // Set email format to HTML
 
   $mail->Subject =  "Mail contact ";
-  $mail->Body    =  "$nom\n $prenom\n $phone\n $msg";
+  $mail->Body    =  "Nom: $nom Prénom: $prenom\n \n $msg";
 
   if(!$mail->send()) {
     echo 'Message could not be sent.';
