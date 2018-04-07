bfac7c623d775b46f24185408dc5a69b06843f9f
Sabrina Nedjah <snedjah78@gmail.com>
2018-04-07 11:46:46 +0200

    modification Readme

--- Readme.md
+++ Readme.md
@@ -1,5 +1,22 @@
-NORMES DE DEV :
+# Talo Energy
 
-Respecter la nomenclature des fichiers /partials
-Les importer dans main.css
-Pas touche au gulp
+
+### Setup
+```bash
+git clone git@github.com:SabrinaNedjah/taloenergy.git
+```
+
+Then open the `index.php` file 
+Need a local server like mamp/wamp
+
+### Features
+- [scrollreveal.js](https://scrollrevealjs.org/)
+- [phpmailer](https://github.com/PHPMailer/PHPMailer)
+- [slick](http://kenwheeler.github.io/slick/)
+
+
+### Technologies
+- HTML
+- CSS
+- JAVASCRIPT - JQUERY
+- Php
--- app/partials/_header.php
+++ app/partials/_header.php
@@ -6,11 +6,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Talo Energy</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
   <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css" />
   <link rel="shortcut icon" href="images/logoTalo.png" type="image/x-icon" />
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="styles/css/main.css" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
 </head>
 
 <body>
